8656e07051452c2d21125df56d15ac6bb3db978f
NeUNekin <NeUNekin@hotmail.com>
2018-09-13 12:13:08 +0700

    check point 3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
-const word = "Hello";
+const word = "Hell";
 class App extends Component {
   render() {
     return (
